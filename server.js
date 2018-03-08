28b92d8f6d4e18b04867bb02f3ad86d07692cd56
olivierhoule <36018416+olivierhoule@users.noreply.github.com>
2018-03-08 13:26:03 -0700

    Add basic /logincheck endpoint
    Added an endpoint for the login form so that a login authentication can be made (wrote comments to explain how that can be done, I don't know how to write the query string)

--- server.js
+++ server.js
@@ -1,13 +1,49 @@
-let http = require("http");
-let fs = require("fs");
-let url = require("url");
-let path = require("path");
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const path = require("path");
+const qs = require("querystring");
+const { Client } = require("pg");
+const db = new Client();
 
-http.createServer(function(req, res) {
+db.connect();
+
+http.createServer((req, res) => {
   let parsedUrl = url.parse(req.url);
-  let pathname = `.${parsedUrl.pathname}`
+  let pathname = `.${parsedUrl.pathname}`;
 
-  fs.exists(pathname, function(exist) {
+  // login
+  if (parsedUrl.path == "/logincheck") {
+    let body = '';
+    req.on('data', (data) => {
+      body += data;
+      // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+      if (body.length > 1e6) {
+        // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+        req.connection.destroy();
+      }
+    });
+    req.on('end', () => {
+      let data = qs.parse(body);
+
+      //check if data.usr and data.pwd are valid
+      db.query('some sort of query string', ['Hello world!'], (err, res) => {
+        console.log(err ? err.stack : res.rows[0].message); // Hello World!
+
+        //if valid login, redirect user to index page
+        // with return res.redirect("/index.html");
+
+        //else, bring back to /login.html with error message?
+        // with return res.redirect("/login.html");
+      });
+
+    });
+    return;
+  }
+
+
+  // file server
+  fs.exists(pathname, (exist) => {
     if (!exist) {
       res.statusCode = 404;
       res.end("File not found");
@@ -18,7 +54,7 @@ http.createServer(function(req, res) {
       pathname += '/index.html';
     }
 
-    fs.readFile(pathname, function(err, data) {
+    fs.readFile(pathname, (err, data) => {
       if (err) {
         res.statusCode = 500;
         res.end("Error getting file");
